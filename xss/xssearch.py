40ea1071dd6e9b00feaa05c19135c46ed154d490
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2023-08-30 10:16:52 -0400

    Update xssearch.py
    Signed-off-by: ibrahim ali <82097150+abrahim7112@users.noreply.github.com>

--- xss/xssearch.py
+++ xss/xssearch.py
@@ -13,8 +13,25 @@ from selenium.common.exceptions import TimeoutException
 import warnings
 import argparse
 import sys
-
-banner = \
+import tkinter
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
+from time import strftime
+from tkinter import ttk
+def all_required():
+    root = tkinter.Tk()
+    root.title("CVE-2021-44790 exploits")
+    root.geometry('800x600+0+0')
+    def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        lbl.config(text = "attack url : "+inp)
+        target = inp
+        scroll_y = Scrollbar(root, orient=VERTICAL)
+        txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+        scroll_y.pack(side=RIGHT, fill=Y)
+        scroll_y.config(command=txtarea.yview)
+        banner = \
     '''            
 	 __   __ _____ _____                     _     
 	 \ \ / // ____/ ____|                   | |    
@@ -28,83 +45,73 @@ banner = \
 
 	   DEVELOPED & OWNED BY : SATHYAPRAKASH SAHOO 
 
-'''
+        '''
 
 # Configuring options for Chrome WebDriver
 
-warnings.filterwarnings('ignore')
+        warnings.filterwarnings('ignore')
 
-options = webdriver.ChromeOptions()
+        options = webdriver.ChromeOptions()
 
-options.add_argument('--headless')
+        options.add_argument('--headless')
 
-options.add_argument('--disable-xss-auditor')
+        options.add_argument('--disable-xss-auditor')
 
-options.add_argument('--disable-web-security')
+        options.add_argument('--disable-web-security')
 
-options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--ignore-certificate-errors')
 
-options.add_argument('--no-sandbox')
+        options.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
-                          chrome_options=options)
+        driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=options)
 
 # Creating arguments for taking input
 
-parser = \
-    argparse.ArgumentParser(epilog='Command : python3 xssearch -u https://www.target.com/search={xss} -p payloads.txt'
-                            )
-
-parser.add_argument('-u', metavar='url.com/?s={xss}',
-                    help='Add URL with paramter as ={xss}',
-                    required=True)
-
-parser.add_argument('-p', metavar='payloads.txt',
-                    help='Add file containing all the payloads',
-                    required=True)
-
-args = parser.parse_args()
-
 # Printing Banner
-
-print (banner)
+        txtarea.insert(END, banner)
+        #print (banner)
 
 # Checking if placeholder is assigned in the URL or not
 
-if not '{xss}' in args.u:
-
-    sys.exit("""
-\033[31mError\033[0m : Need '{xss}' placeholder parameter ! 
-\033[31mMessage\033[0m : python3 xssearch.py -h for more help :) """)
-else:
-
-    target = args.u
-
-p = args.p
-
-print ("[*] Starting XSSearch ...")
+        txtarea.insert(END, "[*] Starting XSSearch ...\n")
+        txtarea.pack(fill=BOTH, expand=True)
+        txtarea.configure(state ='disabled')
+        #print ("[*] Starting XSSearch ...")
 
 # Executing a loop for checking valid XSS payload in the given URL
 
-for payload in open(p, 'r').readlines():
+        for payload in open('xss-injection.txt', 'r').readlines():
 
-    url = target.replace('{xss}', payload)
+            url = target.replace('{xss}', payload)
 
-    driver.get(url)
+            driver.get(url)
 
 # Checking for a javascript pop-up
 
-    try:
+            try:
 
-        WebDriverWait(driver, 1).until(EC.alert_is_present())
+                WebDriverWait(driver, 1).until(EC.alert_is_present())
 
-        alert = driver.switch_to.alert
+                alert = driver.switch_to.alert
 
-        alert.accept()
+                alert.accept()
 
-        print ("\033[31m[+] XSS Triggered !\033[0m", payload)
-    except TimeoutException:
+              #  print ("\033[31m[+] XSS Triggered !\033[0m", payload)
+                txtarea.insert(END, "\033[31m[+] XSS Triggered !\033[0m")
+                txtarea.insert(END, payload)
+            except TimeoutException:
 
-        print ("\033[36m[+] XSS not Triggered ! \033[0m", payload)
+                #print ("\033[36m[+] XSS not Triggered ! \033[0m", payload)
+                txtarea.insert(END, "\033[36m[+] XSS not Triggered ! \033[0m")
+                txtarea.insert(END, payload)
 
-driver.close()
+        driver.close()
+
+    Hacher = Label(root, text="Enter url or domin https://www.example.com/ap/v?search=1 : ").pack()
+    inputtxt = tk.Text(root,height = 1,width = 80)
+    inputtxt.pack()
+    printButton = tk.Button(root,text = "exploit", command = printInput)
+    printButton.pack()
+    lbl = tk.Label(root, text = "")
+    lbl.pack()
+    root.mainloop()
